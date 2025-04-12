0802fba75d8b5354ed65c3fc0cf861a929649a8e
anatolykuznetsov <aikuznetssov@gmail.com>
2025-04-12 15:24:29 -0700

    Initial Commit

---
+++ Kids_motivation/Kids_motivationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kids_motivationApp.swift
+//  Kids_motivation
+//
+//  Created by Anatoly Kuznetsov on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Kids_motivationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Kids_motivationTests/Kids_motivationTests.swift
@@ -0,0 +1,17 @@
+//
+//  Kids_motivationTests.swift
+//  Kids_motivationTests
+//
+//  Created by Anatoly Kuznetsov on 4/12/25.
+//
+
+import Testing
+@testable import Kids_motivation
+
+struct Kids_motivationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
